d1d370753c06af2344384467b176243c8ecfc5aa
ANA PAULA FERREIRA <93100302+fpanap@users.noreply.github.com>
2023-02-19 21:49:05 -0300

    Add files via upload

---
+++ javascript/04.js
@@ -0,0 +1,26 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+console.log(diaSem)
+switch(diaSem){
+    case 0:
+    console.log(`Domingo`)
+    break
+    case 1:
+    console.log(`Segunda-feira`)
+    break
+    case 2:
+    console.log(`Terça-feira`)
+    break
+    case 3:
+    console.log(`Quarta-feira`)
+    break
+    case 4:
+    console.log(`Quinta-feira`)
+    break
+    case 0:
+    console.log(`Sexta-feira`)
+    break
+    case 0:
+    console.log(`Sabado`)
+    default8
+}
